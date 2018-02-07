c5b647558e6c202858adcf5ea9e7bef36650a8a4
Daniel S Knight <daniel.knight@gmail.com>
2018-02-07 14:35:21 -0600

    documentation and removed type parameter.

--- src/main/java/us/sourcefoundry/gutenberg/commands/add/models/ILocationReference.java
+++ src/main/java/us/sourcefoundry/gutenberg/commands/add/models/ILocationReference.java
@@ -1,12 +1,19 @@
 package us.sourcefoundry.gutenberg.commands.add.models;
 
-public interface ILocationReference<T> {
+/**
+ * This is an interface for locations.
+ */
+public interface ILocationReference {
 
+    //The user name.
     String getUser();
 
+    //The data repository.
     String getRepository();
 
+    //The version tag.
     String getTag();
 
+    //The fully qualified name for the reference.
     String toFQN();
 }
